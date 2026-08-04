8c4a883b2717994750b465de680cf6443db2f803
rlagofma1102 <its7637@kau.kr>
2026-08-04 23:12:18 +0900

    fix: ErrorCode_상태메세지_변경

--- src/main/java/com/gaguraczi/paw/domain/todo/exception/code/TagErrorCode.java
+++ src/main/java/com/gaguraczi/paw/domain/todo/exception/code/TagErrorCode.java
@@ -8,10 +8,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum TagErrorCode implements BaseErrorCode {
-    TAG_CREATE_400_1(HttpStatus.CREATED, "TAG_CREATE_400_1", "태그가 생성되지 않았습니다."),
-    TAG_LIST_404_1(HttpStatus.OK, "TAG_LIST_404_1", "태그 목록 조회에 실패했습니다."),
-    TAG_GET_404_2(HttpStatus.OK, "TAG_GET_404_2", "태그 조회에 실패했습니다."),
-    TAG_UPDATE_400_2(HttpStatus.OK, "TAG_UPDATE_400_2", "태그 수정에 실패했습니다.");
+    TAG_CREATE_400_1(HttpStatus.BAD_REQUEST, "TAG_CREATE_400_1", "태그가 생성되지 않았습니다."),
+    TAG_LIST_404_1(HttpStatus.NOT_FOUND, "TAG_LIST_404_1", "태그 목록 조회에 실패했습니다."),
+    TAG_GET_404_2(HttpStatus.NOT_FOUND, "TAG_GET_404_2", "태그 조회에 실패했습니다."),
+    TAG_UPDATE_400_2(HttpStatus.BAD_REQUEST, "TAG_UPDATE_400_2", "태그 수정에 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
