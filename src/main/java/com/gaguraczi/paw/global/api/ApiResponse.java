51ffa29a193f2dff5aa991a6ef597c9a8bb69d49
rlagofma1102 <its7637@kau.kr>
2026-08-04 23:15:32 +0900

    fix: ApiResponse 원래대로 돌려놓기

--- src/main/java/com/gaguraczi/paw/global/api/ApiResponse.java
+++ src/main/java/com/gaguraczi/paw/global/api/ApiResponse.java
@@ -3,7 +3,6 @@ package com.gaguraczi.paw.global.api;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.gaguraczi.paw.domain.todo.dto.response.TagResponse;
 import com.gaguraczi.paw.global.api.code.BaseErrorCode;
 import com.gaguraczi.paw.global.api.code.BaseSuccessCode;
 import lombok.AccessLevel;
@@ -74,8 +73,5 @@ public class ApiResponse<T> { // API 응답
                 data,
                 code.getReason().getHttpStatus()
         );
-    }
-
-    public static ApiResponse<TagResponse> success(TagResponse response) {
     }
 }
