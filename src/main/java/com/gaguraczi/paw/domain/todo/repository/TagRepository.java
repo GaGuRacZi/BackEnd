5deac33f17c6d2cd2366252fa64514e29f409e10
rlagofma1102 <its7637@kau.kr>
2026-08-08 18:25:17 +0900

    fix: String uid 에서 UUID uid로 수정

--- src/main/java/com/gaguraczi/paw/domain/todo/repository/TagRepository.java
+++ src/main/java/com/gaguraczi/paw/domain/todo/repository/TagRepository.java
@@ -6,15 +6,16 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface TagRepository extends JpaRepository<TagEntity, Long> {
 
-    List<TagEntity> findAllByUser_UidOrderByTagNameAsc(String uid);
+    List<TagEntity> findAllByUser_UidOrderByTagNameAsc(UUID uid);
 
-    Optional<TagEntity> findByTagIdAndUser_Uid(Long tagId, String uid);
+    Optional<TagEntity> findByTagIdAndUser_Uid(Long tagId, UUID uid);
 
-    boolean existsByUser_UidAndTagName(String uid, String tagName);
+    boolean existsByUser_UidAndTagName(UUID uid, String tagName);
 
-    boolean existsByUser_UidAndTagNameAndTagIdNot(String uid, String tagName, Long tagId);
+    boolean existsByUser_UidAndTagNameAndTagIdNot(UUID uid, String tagName, Long tagId);
 }
