8575c6415447bd82fa547838fc4bc14b1f93aa6a
rlagofma1102 <its7637@kau.kr>
2026-08-09 16:42:49 +0900

    fix: TagService에 맞춰 controller, errorcode 수정

--- src/main/java/com/gaguraczi/paw/domain/todo/controller/TagController.java
+++ src/main/java/com/gaguraczi/paw/domain/todo/controller/TagController.java
@@ -1,15 +1,14 @@
 package com.gaguraczi.paw.domain.todo.controller;
 
-import com.gaguraczi.paw.domain.todo.exception.code.TagSuccessCode;
 import com.gaguraczi.paw.domain.todo.dto.request.TagCreateRequest;
 import com.gaguraczi.paw.domain.todo.dto.request.TagUpdateRequest;
 import com.gaguraczi.paw.domain.todo.dto.response.TagResponse;
+import com.gaguraczi.paw.domain.todo.exception.code.TagSuccessCode;
 import com.gaguraczi.paw.domain.todo.service.TagService;
 import com.gaguraczi.paw.global.api.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -24,10 +23,9 @@ public class TagController {
 
     @PostMapping
     public ResponseEntity<ApiResponse<TagResponse>> createTag(
-            @AuthenticationPrincipal String uid,
             @Valid @RequestBody TagCreateRequest request
     ) {
-        TagResponse response = tagService.createTag(uid, request);
+        TagResponse response = tagService.create(request);
 
         return ResponseEntity
                 .created(URI.create("/api/tags/" + response.tagId()))
@@ -35,47 +33,32 @@ public class TagController {
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse<List<TagResponse>>> getMyTags(
-            @AuthenticationPrincipal String uid
-    ) {
-        List<TagResponse> responses = tagService.getTagsByUser(uid);
-
+    public ResponseEntity<ApiResponse<List<TagResponse>>> getMyTags() {
         return ResponseEntity.ok(
-                ApiResponse.onSuccess(TagSuccessCode.TAG_LIST_200, responses)
+                ApiResponse.onSuccess(TagSuccessCode.TAG_LIST_200, tagService.getMyTags())
         );
     }
 
     @GetMapping("/{tagId}")
-    public ResponseEntity<ApiResponse<TagResponse>> getTag(
-            @AuthenticationPrincipal String uid,
-            @PathVariable Long tagId
-    ) {
-        TagResponse response = tagService.getTag(uid, tagId);
-
+    public ResponseEntity<ApiResponse<TagResponse>> getTag(@PathVariable Long tagId) {
         return ResponseEntity.ok(
-                ApiResponse.onSuccess(TagSuccessCode.TAG_GET_200, response)
+                ApiResponse.onSuccess(TagSuccessCode.TAG_GET_200, tagService.getTag(tagId))
         );
     }
 
     @PatchMapping("/{tagId}")
     public ResponseEntity<ApiResponse<TagResponse>> updateTag(
-            @AuthenticationPrincipal String uid,
             @PathVariable Long tagId,
             @Valid @RequestBody TagUpdateRequest request
     ) {
-        TagResponse response = tagService.updateTag(uid, tagId, request);
-
         return ResponseEntity.ok(
-                ApiResponse.onSuccess(TagSuccessCode.TAG_UPDATE_200, response)
+                ApiResponse.onSuccess(TagSuccessCode.TAG_UPDATE_200, tagService.updateTag(tagId, request))
         );
     }
 
     @DeleteMapping("/{tagId}")
-    public ResponseEntity<Void> deleteTag(
-            @AuthenticationPrincipal String uid,
-            @PathVariable Long tagId
-    ) {
-        tagService.deleteTag(uid, tagId);
+    public ResponseEntity<Void> deleteTag(@PathVariable Long tagId) {
+        tagService.deleteTag(tagId);
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/gaguraczi/paw/domain/todo/exception/code/TagErrorCode.java
+++ src/main/java/com/gaguraczi/paw/domain/todo/exception/code/TagErrorCode.java
@@ -11,6 +11,7 @@ public enum TagErrorCode implements BaseErrorCode {
     TAG_CREATE_400_1(HttpStatus.BAD_REQUEST, "TAG_CREATE_400_1", "태그가 생성되지 않았습니다."),
     TAG_LIST_404_1(HttpStatus.NOT_FOUND, "TAG_LIST_404_1", "태그 목록 조회에 실패했습니다."),
     TAG_GET_404_2(HttpStatus.NOT_FOUND, "TAG_GET_404_2", "태그 조회에 실패했습니다."),
+    TAG_DELETE_404_3(HttpStatus.NOT_FOUND, "TAG_DELETE_404_1", "태그 삭제에 실패했습니다."),
     TAG_UPDATE_400_2(HttpStatus.BAD_REQUEST, "TAG_UPDATE_400_2", "태그 수정에 실패했습니다.");
 
     private final HttpStatus httpStatus;
